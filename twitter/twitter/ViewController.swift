33005496684cd14939ab0c0d5b571a081fadbee3
clittle1129 <46544699+clittle1129@users.noreply.github.com>
2019-02-14 13:47:13 -0500

    Add files via upload

---
+++ twitter/twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by Christian Alec Littleton on 2/12/19.
+//  Copyright © 2019 Christian Alec Littleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ twitter/twitterTests/twitterTests.swift
@@ -0,0 +1,34 @@
+//
+//  twitterTests.swift
+//  twitterTests
+//
+//  Created by Christian Alec Littleton on 2/12/19.
+//  Copyright © 2019 Christian Alec Littleton. All rights reserved.
+//
+
+import XCTest
+@testable import twitter
+
+class twitterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
